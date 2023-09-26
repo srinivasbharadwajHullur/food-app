3fe5ff38989691d6ef51bc50cf716ae68b3b8987
srinivasbharadwaj8 <srinivasbharadwaj8@gmail.com>
2023-09-26 15:11:28 +0530

    Added the necessary uris

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,7 @@ export const LEFT_ARROW_URI = "https://cdn-icons-png.flaticon.com/512/1103/11034
 export const RIGHT_ARROW_URI = "https://cdn-icons-png.flaticon.com/512/54/54661.png"
 export const WHATS_ON_YOUR_MIND_IMAGES = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
 export const RESTAURANT_ITEM_IMAGE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const RESTAURANT_MENU_URI = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9181764&lng=77.5504122&catalog_qa=undefined&submitAction=ENTER&restaurantId="
+export const UP_ARROW_URI = "https://cdn-icons-png.flaticon.com/512/60/60564.png"
+export const DOWN_ARROW_URI = "https://cdn-icons-png.flaticon.com/512/159/159119.png"
+export const RESTAURANT_ITEM_IMAGE_URI = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
