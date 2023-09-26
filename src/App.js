89e4470d008d5e18a35c53dbf0381cfe3c4f7cff
srinivasbharadwaj8 <srinivasbharadwaj8@gmail.com>
2023-09-26 15:09:08 +0530

    Added the React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Body from './components/Body';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import RestaurantMenu from './components/RestaurantMenu';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Routes>
+        <Route path='/' element={<Body />}/>
+        <Route path='/restaurantmenu/:id' element={<RestaurantMenu />}/>
+      </Routes>
     </div>
   );
 }
