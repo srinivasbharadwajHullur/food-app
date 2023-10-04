7f6f71c5bd55be576e2e47a397f72060eca64304
srinivasbharadwaj8 <srinivasbharadwaj8@gmail.com>
2023-10-04 12:00:18 +0530

    Added the logic to display the count of the items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { LOGO_URI, USER_ICON, CART_URI } from "../utils/constants";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
   const goToHome = () => {
     navigate("/")
+  }
+
+  const cartItems = useSelector(state => state.cart.cartItems)
+  let count = 0;
+  for(let i=0;i<=cartItems.length-1;i++) {
+    count = count + cartItems[i].quantity
+  }
+  
+
+  const goToCartPage = () => {
+    navigate("/cart");
   }
   return (
     <div className="flex justify-around items-center shadow-md">
@@ -28,8 +40,8 @@ const Header = () => {
           alt="User Icon"
         />
         <div className="flex items-center">
-          <img className="w-10 cursor-pointer" src={CART_URI} alt="Cart Icon" />
-          <p> - 0 </p>
+          <img onClick={goToCartPage} className="w-10 cursor-pointer" src={CART_URI} alt="Cart Icon" />
+          <p> - {count} </p>
         </div>
       </div>
     </div>
