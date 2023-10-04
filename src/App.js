4b09eca7cf41c7937150db41669ffb4d0fc99d2f
srinivasbharadwaj8 <srinivasbharadwaj8@gmail.com>
2023-10-04 11:59:32 +0530

    Added the Route for the cartpage and success page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Body from './components/Body';
 import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom';
 import RestaurantMenu from './components/RestaurantMenu';
+import CartPage from './components/CartPage';
+import SuccessPage from './components/SuccessPage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Body />}/>
         <Route path='/restaurantmenu/:id' element={<RestaurantMenu />}/>
+        <Route path='/cart' element={<CartPage />}/>
+        <Route path='/success' element={<SuccessPage />}/>
       </Routes>
     </div>
   );
